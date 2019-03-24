49dad6645a75f23774328defe04e75d9de9e93e6
KIMINJOONG <ijboym216@gmail.com>
2019-03-24 15:23:11 +0900

    Getting data from the GraphQL API part Two

--- src/queries.js
+++ src/queries.js
@@ -1,11 +1,12 @@
 import ggl from "graphql-tag";
 
-export const HOME_PAGE = qql`
+export const HOME_PAGE = ggl`
     {
         movies(limit: 50, rating: 7) {
             id
             title
             genres
+            rating
         }
     }
 `;
